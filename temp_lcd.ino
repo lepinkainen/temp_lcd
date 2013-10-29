d7ac051040d1420c9b548c531d72cb370f3884eb
Riku Lindblad <riku.lindblad@gmail.com>
2013-10-29 23:36:46 +0200

    Actual working version
    Display 1-N dallas one wire sensor values on the LCD screen

--- temp_lcd.ino
+++ temp_lcd.ino
@@ -14,6 +14,9 @@ The circuit:
 
 // include the library code:
 #include <LiquidCrystal.h>
+#include <OneWire.h>
+
+OneWire  ds(2);  // pin 2
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -40,51 +43,107 @@ void setup() {
 }
 
 void loop() {
-  // read a line from the serial interface and insert it to the correct line on the display
-  if(Serial.available() > 0) {
-    char c = Serial.read();
-    Serial.print(c);
-    
-    if(c == '\n') {
-      lcd.print(incomingString);
-      incomingString = "";
-      if(row == 0) { row = 1; } else { row = 0; }
-      lcd.setCursor(0,row);
-      Serial.println(row);
-    } 
-    else {
-      incomingString += c;
-    }
+  lcd.setCursor(0,0);
+  
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+  
+  if ( !ds.search(addr)) {
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  
+  for( i = 0; i < 8; i++) {
+    lcd.print(addr[i], HEX);
   }
 
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      lcd.print("CRC is not valid!");
+      return;
+  }
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      type_s = 1;
+      break;
+    case 0x28:
+      type_s = 0;
+      break;
+    case 0x22:
+      type_s = 0;
+      break;
+    default:
+      return;
+  } 
 
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1);         // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+  
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+  }
+  
+  // convert the data to actual temperature
+  unsigned int raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // count remain gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    if (cfg == 0x00) raw = raw << 3;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw << 2; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw << 1; // 11 bit res, 375 ms
+    // default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  lcd.setCursor(0,1);
+  Serial.println(celsius);
+  lcd.print(celsius);
+  lcd.print(" C");
 
   ButtonVoltage = analogRead(0);
-
   ButtonPressed = ButtonVoltage >> 7; //this removes least significant bits to get more stable reads.
 
   switch (ButtonPressed)
   {
-  case 0:
+  case 0: // right
+   Serial.println("right");
     break; 
-  case 1: 
+  case 1: // up
+   Serial.println("up");
+   break;
+  case 2: // down
+  Serial.println("down");
     break;
-  case 2:
-    fadeValue = fadeValue -5;
-    if (fadeValue == 254) 
-    {
-      fadeValue = 255;
-    }
-    analogWrite (Backlight, fadeValue);
-    delay (100);
+  case 3: // left
+  Serial.println("left");
     break;
-  case 3:
+  case 5: // select
+  Serial.println("select");
     break;
-  case 5:
-    break;
-  case 7:
+  case 7: // nothing pressed
+  Serial.println("none");
     break;
   }
+  
+  delay(1000);
 }
 
 
